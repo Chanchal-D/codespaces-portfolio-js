b5d31edd64435d0e11c49d772632863fb46158a0
Chanchal-D <117251667+Chanchal-D@users.noreply.github.com>
2023-10-19 02:18:15 +0530

    add alt changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Chanchal Kuntal",
+  title: "Web Developer & Content Creator",
+  email: "chanchalkuntal398@gmail.com",
+  gitHub: "Chanchal-D",
+  instagram: "Chanchal_Kuntal",
+  linkedIn: "Chanchal-Kuntal",
+  medium: "chanchalkuntak398",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,29 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Basic Git Commands — How to Use Git in a Real Project",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Create a beginner friendly article to help explain Git commands and how to get started.",
+    url: "https://medium.com/@chanchalkuntal398/basic-git-commands-how-to-use-git-in-a-real-project-c4a793971c77",
   },
   {
-    title: "Web Development for Beginners",
+    title: "An introduction to HTML for Beginners",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Complete revision of Html and how to start your journey of web development.",
+    url: "https://medium.com/@chanchalkuntal398/an-introduction-to-html-for-beginners-6e40ed8aaa24",
   },
   {
-    title: "My Resume Site",
+    title: "NFT-MANIA",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "NFT-MANIA is a fully responsive NFT website,
+      Responsive for all devices, built using HTML, CSS, and JavaScript.",
+    url: "https://chanchal-d.github.io/NFT-MANIA/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Review pull request",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Repo designed for new github users to learn how to review/pull request",
+    url: "https://github.com/Chanchal-D/skills-review-pull-requests",
   },
 ];
 
